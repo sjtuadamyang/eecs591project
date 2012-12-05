cc03ebf617ba81eef0cb8bfaa9b09d2af2323677
andymacool <hongyuwang87@gmail.com>
2012-12-05 03:18:33 -0500

    a

--- 591demo/upload_triput.php
+++ 591demo/upload_triput.php
@@ -12,7 +12,7 @@
   echo prep_put("photoinfo", $photoid, $values, "null"); //should be tri_put
 
   $values=array("content"=>$imagedata);
-  echo prep_put("photo_l", $photoid, $values, "null"); //should be tri_put  
+  echo prep_put("photo_l", $photoid, $values, "photo"); //should be tri_put  
 
 /*
   $values=array("username"=>$username, "content"=>$imagedata, "time"=>$currtime);
