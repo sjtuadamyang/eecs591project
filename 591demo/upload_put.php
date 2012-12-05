03657683eafc67d2df50e29252d1b5fd34b276a3
dozenow <dozenow@ubuntu.ubuntu-domain>
2012-12-04 22:53:31 -0500

    demo

--- 591demo/upload_put.php
+++ 591demo/upload_put.php
@@ -3,11 +3,11 @@
 
   $username = $_POST['username'];
   $photoid  = $_POST['photoid'];
-  //$imageraw = $_POST['imageraw'];
-  $imageraw = 'iVBORw0KGgoAAAANSUhEUgAAABwAAAASCAMAAAB/2U7WAAAABl'
-              . 'BMVEUAAAD///+l2Z/dAAAASUlEQVR4XqWQUQoAIAxC2/0vXZDr'
-              . 'EX4IJTRkb7lobNUStXsB0jIXIAMSsQnWlsV+wULF4Avk9fLq2r'
-              . '8a5HSE35Q3eO2XP1A1wQkZSgETvDtKdQAAAABJRU5ErkJggg==';
+  $imageraw = $_POST['imageraw'];
+  //$imageraw = 'iVBORw0KGgoAAAANSUhEUgAAABwAAAASCAMAAAB/2U7WAAAABl'
+    //          . 'BMVEUAAAD///+l2Z/dAAAASUlEQVR4XqWQUQoAIAxC2/0vXZDr'
+      //        . 'EX4IJTRkb7lobNUStXsB0jIXIAMSsQnWlsV+wULF4Avk9fLq2r'
+        //      . '8a5HSE35Q3eO2XP1A1wQkZSgETvDtKdQAAAABJRU5ErkJggg==';
 
   $currtime  = time() % 3600;
   $imageraw  = base64_decode($imageraw);
